522fb363cf72c182d37347469fddef447377cfb9
Dominus-Proxius <162509065+Dominus-Proxius@users.noreply.github.com>
2024-07-04 20:58:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+function setTheme(theme) {
+    if (theme === 'dark') {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+}
 
+function setTabCloak(cloak) {
+    if (cloak === 'Work') {
+        document.title = 'Work - Important Document';
+    } else if (cloak === 'Study') {
+        document.title = 'Study - Research Paper';
+    } else {
+        document.title = 'Home';
+    }
+}
